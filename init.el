e2491e01bf26385723af9a03a12373ad032e9d32
Stefan Widgren <stefan.widgren@gmail.com>
2018-11-07 07:37:26 +0100

    Add 'https://orgmode.org/elpa/' to the package archives list

--- init.el
+++ init.el
@@ -10,6 +10,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
