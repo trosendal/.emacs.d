e7e9eda8b68ac13efe8c4e9b2974f7d63d678381
Thomas Rosendal <thomas.rosendal@sva.se>
2020-08-17 13:43:37 +0200

    * config.org: ADD company

--- init.el
+++ init.el
@@ -34,7 +34,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages (quote (counsel helm-pass ess magit diminish use-package))))
+ '(package-selected-packages
+   (quote
+    (company counsel helm-pass ess magit diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
