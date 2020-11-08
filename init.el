0137a301eee8b25480f13ad284d0a96f8a2bb1dc
Thomas Rosendal <thomas.rosendal@sva.se>
2020-11-08 16:40:16 +0100

    a commit

--- init.el
+++ init.el
@@ -33,8 +33,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages (quote (counsel helm-pass ess magit diminish use-package))))
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(package-selected-packages '(counsel helm-pass ess magit diminish use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
