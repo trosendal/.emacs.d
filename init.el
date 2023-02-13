1105b6af4d09b478ca6341792f77ece68c7bb9a2
Thomas Rosendal <thomas.rosendal@sva.se>
2023-02-13 16:18:01 +0100

    add rust

--- init.el
+++ init.el
@@ -34,7 +34,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(initial-frame-alist '((fullscreen . maximized)))
- '(package-selected-packages '(counsel helm-pass ess magit diminish use-package)))
+ '(package-selected-packages
+   '(rust-mode cargo-mode counsel helm-pass ess magit diminish use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
